b778f4caac60b980c11478f9e257b5c596b4eb1b
rlorigro <rlorigro@ucsc.edu>
2023-09-29 16:36:08 -0700

    Finish implementing and testing interval graph

--- inc/IntervalGraph.hpp
+++ inc/IntervalGraph.hpp
@@ -4,6 +4,8 @@
 
 #include <unordered_map>
 #include <unordered_set>
+#include <functional>
+#include <stdexcept>
 #include <algorithm>
 #include <ostream>
 #include <utility>
@@ -15,7 +17,10 @@
 
 using std::unordered_map;
 using std::unordered_set;
+using std::runtime_error;
+using std::to_string;
 using std::set_union;
+using std::function;
 using std::ostream;
 using std::vector;
 using std::string;
@@ -33,35 +38,99 @@ using interval_t = pair<int64_t,int64_t>;
 template<class T> class IntervalNode{
 public:
     unordered_set<T> values;
-    unordered_set<uint64_t> neighbors;
+    unordered_set<interval_t> neighbors;
 
-    IntervalNode<T>()=default;
-    explicit IntervalNode<T>(const unordered_set<T>& values);
+    IntervalNode()=default;
+    explicit IntervalNode<T>(const unordered_set<T>& value);
+    void update_values(const unordered_set<T>& value);
 };
 
 
-template <class T> IntervalNode<T>::IntervalNode(const unordered_set<T>& v):
-    values(),
-    neighbors()
-{
-    for(const auto& item: v){
-        values.insert(item);
+template <class T> void IntervalNode<T>::update_values(const unordered_set<T>& value){
+    for(const auto& v: value){
+        values.emplace(v);
     }
 }
+
+
+template <class T> IntervalNode<T>::IntervalNode(const unordered_set<T>& value):
+    values(value),
+    neighbors()
+{}
 
 
 template <class T> class IntervalGraph {
     unordered_map <interval_t, IntervalNode<T> > nodes;
 
 public:
-    IntervalGraph<T>(vector <pair <interval_t,unordered_set<T> > >& intervals);
+    explicit IntervalGraph<T>(vector <pair <interval_t,unordered_set<T> > >& intervals);
     void get_connected_components(vector <unordered_set<interval_t> >& components);
-    void print(ostream& out);
+    void for_value_in_interval(const interval_t& interval, const function<void(const T& value)>& f) const;
+    IntervalNode<T> get_node(const interval_t& interval);
 };
 
 
-template <class T> void IntervalGraph<T>::print(ostream& out) {
+template <class T> IntervalGraph<T>::IntervalGraph(vector <pair <interval_t,unordered_set<T> > >& labeled_intervals){
+    // How to sort labeled intervals with structure ((a,b), label) by start (a)
+    auto left_comparator = [](const pair <interval_t,unordered_set<T> >& a, const pair <interval_t,unordered_set<T> >& b){
+        return a.first.first < b.first.first;
+    };
 
+    // How to sort intervals with structure (a,b) by end (b)
+    auto right_comparator = [](const interval_t& a, const interval_t& b){
+        return a.second < b.second;
+    };
+
+    sort(labeled_intervals.begin(), labeled_intervals.end(), left_comparator);
+
+    // Only need to store/compare the interval (and not the data/label) for this DS
+    set<interval_t, decltype(right_comparator)> active_intervals;
+
+    for (const auto& [interval,value] : labeled_intervals){
+        if (interval.first > interval.second){
+            throw runtime_error("ERROR: interval start is greater than interval stop: " + to_string(interval.first) + ',' + to_string(interval.second));
+        }
+
+        // Add the interval nodes to the graph, mapped by their interval (pair)
+        auto [iter,success] = nodes.try_emplace(interval,value);
+        if (not success){
+            iter->second.update_values(value);
+        }
+
+        vector<interval_t> to_be_removed;
+
+        // Iterate active intervals, which are maintained sorted by interval stop
+        for (const auto& other_interval: active_intervals){
+            // Flag any expired intervals for deletion
+            if (other_interval.second < interval.first){
+                to_be_removed.emplace_back(other_interval);
+            }
+            // Add edges from all active intervals ot the current interval
+            else{
+                nodes.at(other_interval).neighbors.emplace(interval);
+                nodes.at(interval).neighbors.emplace(other_interval);
+            }
+        }
+
+        // Remove the intervals that have been passed already in the sweep
+        for (const auto& item: to_be_removed){
+            active_intervals.erase(item);
+        }
+
+        active_intervals.emplace(interval);
+    }
+}
+
+
+template <class T> IntervalNode<T> IntervalGraph<T>::get_node(const interval_t& interval){
+    return nodes.at(interval);
+}
+
+
+template <class T> void IntervalGraph<T>::for_value_in_interval(const interval_t& interval, const function<void(const T& value)>& f) const{
+    for (const auto& value: nodes.at(interval).values){
+        f(value);
+    }
 }
 
 
@@ -101,48 +170,6 @@ template <class T> void IntervalGraph<T>::get_connected_components(vector <unord
 }
 
 
-template <class T> IntervalGraph<T>::IntervalGraph(vector <pair <interval_t,unordered_set<T> > >& intervals) :
-        nodes()
-{
-    auto left_comparator = [](pair <interval_t,unordered_set<T> >& a, pair <interval_t,unordered_set<T> >& b){
-        return a.first.first < b.first.first;
-    };
-
-    auto right_comparator = [](pair <interval_t,unordered_set<T> >& a, pair <interval_t,unordered_set<T> >& b){
-        return a.first.second < b.first.second;
-    };
-
-    sort(intervals.begin(), intervals.end(), left_comparator);
-
-    set<interval_t, decltype(right_comparator)> active_intervals;
-
-    for (const auto& [interval,value] : intervals){
-        // Add the interval nodes to the graph, mapped by their interval (pair)
-        auto result = nodes.try_emplace(interval,IntervalNode(value));
-
-        vector<interval_t> to_be_removed;
-
-        // Iterate active intervals, which are maintained sorted by interval stop
-        for (const auto& other_interval: active_intervals){
-            // Flag any expired intervals for deletion
-            if (other_interval.second < interval.first){
-                to_be_removed.emplace_back(other_interval);
-            }
-            // Add edges from all active intervals ot the current interval
-            else{
-                nodes.at(other_interval).neighbors.emplace(interval);
-                nodes.at(interval).neighbors.emplace(other_interval);
-            }
-        }
-
-        // Remove the intervals that have been passed already in the sweep
-        for (const auto& item: to_be_removed){
-            active_intervals.erase(item);
-        }
-
-        active_intervals.emplace(interval);
-    }
-}
 
 
 
--- src/test/test_interval_graph.cpp
+++ src/test/test_interval_graph.cpp
@@ -2,29 +2,164 @@
 #include <stdexcept>
 
 using std::runtime_error;
+using std::exception;
 using std::cerr;
 
 
 #include "IntervalGraph.hpp"
+#include "pair_hash.hpp"
 
 using sv_merge::IntervalNode;
 using sv_merge::IntervalGraph;
 using sv_merge::interval_t;
 
 
-int main(){
-    vector <pair <interval_t,unordered_set<string> > > intervals = {
-            {{0,10}, {"a"}},
-            {{0,10}, {"b"}},
-            {{0,10}, {"c"}},
-            {{10,20}, {"d"}},
-            {{11,20}, {"e"}},
-            {{11,0}, {"f"}},
+template <class T> void print_component(const IntervalGraph<T>& g, const unordered_set <interval_t>& component){
+    for (const auto& interval: component){
+        cerr << '\t' << interval.first << ',' << interval.second << ' ';
+
+        g.for_value_in_interval(interval, [&](const T& value){
+            cerr << value << ',';
+        });
+
+        cerr << '\n';
+    }
+}
+
+
+template <class T> void print_components(const IntervalGraph<T>& g, const vector <unordered_set <interval_t> >& components){
+    int c = 0;
+    for (const auto& component: components){
+        cerr << c++ << '\n';
+        print_component(g, component);
+        cerr << '\n';
+    }
+}
+
+
+void test_standard(){
+    vector <pair <interval_t,unordered_set<string> > > labeled_intervals = {
+        {{-1,9}, {"a"}},    // 0
+        {{0,10}, {"b"}},    // 0 collapse v
+        {{0,10}, {"c"}},    // 0 collapse ^
+        {{10,20}, {"d"}},   // 0
+        {{11,20}, {"e"}},   // 0
+        {{21,25}, {"f"}},   // 1
+        {{22,26}, {"g"}}    // 1
     };
 
-    IntervalGraph<string> g(intervals);
+    IntervalGraph<string> g(labeled_intervals);
+
+    vector <unordered_set <interval_t> > components;
+
+    g.get_connected_components(components);
+
+    vector <unordered_set <interval_t> > expected_result = {
+        {
+            {-1, 9},
+            {0, 10},
+            {10, 20},
+            {11, 20}
+        },
+        {
+            {21, 25},
+            {22, 26},
+        }
+    };
+
+    unordered_map <interval_t, unordered_set<string> > expected_values = {
+            {{-1, 9},{"a"}},
+            {{0, 10},{"b", "c"}},
+            {{10, 20},{"d"}},
+            {{11, 20},{"e"}},
+            {{21, 25},{"f"}},
+            {{22, 26},{"g"}}
+    };
+
+    int c = 0;
+    for (const auto& component: components){
+        cerr << "searching for matching component: " << c << '\n';
+        print_component(g, component);
+
+        bool pass = false;
+
+        // Check that the components are structured as expected
+        auto iter = expected_result.begin();
+        while (iter != expected_result.end()){
+            if (component == *iter){
+                cerr << "found" << '\n';
+                pass = true;
+                break;
+            }
+            iter++;
+        }
+
+        expected_result.erase(iter);
+
+        if (not pass){
+            throw runtime_error("FAIL: components not expected");
+        }
+
+        // Check that all the values for each node in the component are as expected
+        pass = true;
+        for (const auto& interval: component){
+            auto n = g.get_node(interval);
+
+            auto result = expected_values.find(interval);
+
+            if (result != expected_values.end()){
+                if (result->second != n.values){
+                    pass = false;
+                }
+            }
+            else{
+                pass = false;
+            }
+        }
+
+        if (not pass){
+            throw runtime_error("FAIL: values not expected");
+        }
+
+        c++;
+    }
+
+    cerr << "PASS: standard" << '\n';
+}
 
 
+void test_bad_interval(){
+    vector <pair <interval_t,unordered_set<string> > > labeled_intervals = {
+        {{0,10}, {"a"}},
+        {{0,10}, {"b"}},
+        {{0,10}, {"c"}},
+        {{10,20}, {"d"}},
+        {{11,20}, {"e"}},
+        {{11,0}, {"f"}}
+    };
+
+    try {
+        IntervalGraph<string> g(labeled_intervals);
+    }
+    catch (exception& e){
+        auto expected_error = "ERROR: interval start is greater than interval stop";
+        string error = e.what();
+
+
+        if (error.find(expected_error) != std::string::npos){
+            cerr << "PASS: bad_interval" << '\n';
+        }
+        else{
+            cerr << e.what() << '\n';
+            throw runtime_error("FAIL: unexpected error");
+        }
+    }
+}
+
+
+int main(){
+    test_standard();
+    test_bad_interval();
 
     return 0;
 }
