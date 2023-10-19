d6117a820ad44894ef933e6545d104b422c2cbfc
rlorigro <rlorigro@ucsc.edu>
2023-10-18 19:04:59 -0700

    Begin TransMap

--- inc/TransitiveMap.hpp
+++ inc/TransitiveMap.hpp
@@ -17,14 +17,101 @@ using std::pair;
 
 namespace sv_merge {
 
-
-template <class T> class TransitiveMap {
+class TransMapNode: public Node {
 public:
-    /// Attributes
-    Graph<T> graph;
+    string name;
+    char type;
 
-
-
+    explicit TransMapNode(const string& name);
+    explicit TransMapNode(string& name);
 };
+
+
+TransMapNode::TransMapNode(const string& name):
+        Node(name),
+        type('*')
+{}
+
+
+TransMapNode::TransMapNode(string& name):
+        Node(name),
+        type('*')
+{}
+
+
+class TransMap {
+    /// Attributes
+    Graph<TransMapNode> graph;
+    const string sample_node_name;
+    const string read_node_name;
+    const string path_node_name;
+
+public:
+    TransMap();
+    void add_sample(const string& name);
+    void add_read(const string& name);
+    void add_path(const string& name);
+    void add_edge(const string& a, const string& b);
+    void add_edge(const string& a, const string& b, float weight);
+
+    const TransMapNode& get_read_sample(const string& read_id);
+    void for_each_read_of_sample(const string& sample_name, const function<void(const TransMapNode& node)>& f);
+    void for_each_sample_of_read(const string& read_name, const function<void(const TransMapNode& node)>& f);
+    void for_each_sample_of_path(const string& path_name, const function<void(const TransMapNode& node)>& f);
+};
+
+
+TransMap::TransMap():
+    sample_node_name("sample_node"),
+    read_node_name("read_node"),
+    path_node_name("path_node")
+{
+    auto& sample_node = graph.add_node(sample_node_name);
+    sample_node.type = 'S';
+
+    auto& read_node = graph.add_node(read_node_name);
+    read_node.type = 'R';
+
+    auto& path_node = graph.add_node(path_node_name);
+    path_node.type = 'P';
+}
+
+
+void TransMap::add_read(const string& name){
+    if (name == read_node_name or name == sample_node_name or name == path_node_name){
+        throw runtime_error("ERROR: cannot add node with preset node name: " + name);
+    }
+    auto& node = graph.add_node(name);
+    node.type;
+}
+
+
+void TransMap::add_sample(const string& name){
+    if (name == read_node_name or name == sample_node_name or name == path_node_name){
+        throw runtime_error("ERROR: cannot add node with preset node name: " + name);
+    }
+    auto& node = graph.add_node(name);
+    node.type;
+}
+
+
+void TransMap::add_path(const string& name){
+    if (name == read_node_name or name == sample_node_name or name == path_node_name){
+        throw runtime_error("ERROR: cannot add node with preset node name: " + name);
+    }
+    auto& node = graph.add_node(name);
+    node.type;
+}
+
+
+void TransMap::add_edge(const string& a, const string& b){
+    graph.add_edge(a,b,0);
+}
+
+
+void TransMap::add_edge(const string& a, const string& b, float weight){
+    graph.add_edge(a,b,weight);
+}
+
 
 }
