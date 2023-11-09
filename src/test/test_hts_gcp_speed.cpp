a36b5fe215e462d0eb1f717802a633e5782b1489
rlorigro <rlorigro@ucsc.edu>
2023-11-09 15:50:48 -0800

    More types of iteration

--- src/test/test_hts_gcp_speed.cpp
+++ src/test/test_hts_gcp_speed.cpp
@@ -4,6 +4,7 @@
 #include "misc.hpp"
 #include "bed.hpp"
 #include "bam.hpp"
+#include "hts.h"
 
 #include <unordered_map>
 #include <exception>
@@ -32,7 +33,7 @@ using namespace sv_merge;
 
 
 //
-void iterate_regions(path bam_path, vector<char*> regions){
+void iterate_region(path bam_path, string region){
     GoogleAuthenticator authenticator;
     authenticator.update();
 
@@ -45,27 +46,31 @@ void iterate_regions(path bam_path, vector<char*> regions){
     bam_index = nullptr;
     alignment = bam_init1();
 
-
+    Timer t;
+    cerr << t << "Begin" << '\n';
     if ((bam_file = hts_open(bam_path.string().c_str(), "r")) == nullptr) {
         throw runtime_error("ERROR: Cannot open bam file: " + bam_path.string());
     }
+    cerr << t << "hts_open" << '\n';
 
     // bam index
     if ((bam_index = sam_index_load(bam_file, bam_path.string().c_str())) == nullptr) {
         throw runtime_error("ERROR: Cannot open index for bam file: " + bam_path.string() + "\n");
     }
+    cerr << t << "sam_index_load" << '\n';
 
     // bam header
     if ((bam_header = sam_hdr_read(bam_file)) == nullptr){
         throw runtime_error("ERROR: Cannot open header for bam file: " + bam_path.string() + "\n");
     }
+    cerr << t << "sam_hdr_read" << '\n';
 
-    hts_name2id_f getid;
+    hts_name2id_f getid = (hts_name2id_f)bam_name2id;
     int r_count;
 
-    hts_reglist_t* reglist = hts_reglist_create(regions.data(), int(regions.size()), &r_count, bam_header,  getid);
+    hts_itr_t *itr = sam_itr_querys(bam_index, bam_header, region.c_str());
 
-    hts_itr_t *itr = sam_itr_regions(bam_index, bam_header, reglist, r_count);
+    cerr << t << "sam_itr_region" << '\n';
 
     while (sam_itr_next(bam_file, itr, alignment) >= 0) {
         if (alignment->core.tid < 0) {
@@ -78,15 +83,148 @@ void iterate_regions(path bam_path, vector<char*> regions){
         auto pos = alignment->core.pos;
         auto len = bam_cigar2rlen(int(alignment->core.n_cigar), bam_get_cigar(alignment));
 
-        cerr << ref_name << ' ' << pos << ' ' << pos + len << ' ' << read_name << '\n';
+//        cerr << ref_name << ' ' << pos << ' ' << pos + len << ' ' << read_name << '\n';
     }
+
+    cerr << t << "Done iterating" << '\n';
 
     bam_destroy1(alignment);
     hts_itr_destroy(itr);
     bam_hdr_destroy(bam_header);
     hts_close(bam_file);
     hts_idx_destroy(bam_index);
+}
 
+
+
+//
+void iterate_regions(path bam_path, vector<char*>& regions){
+    GoogleAuthenticator authenticator;
+    authenticator.update();
+
+    samFile* bam_file;
+    bam_hdr_t* bam_header;
+    hts_idx_t* bam_index;
+    bam1_t* alignment;
+
+    bam_file = nullptr;
+    bam_index = nullptr;
+    alignment = bam_init1();
+
+    Timer t;
+    cerr << t << "Begin" << '\n';
+    if ((bam_file = hts_open(bam_path.string().c_str(), "r")) == nullptr) {
+        throw runtime_error("ERROR: Cannot open bam file: " + bam_path.string());
+    }
+    cerr << t << "hts_open" << '\n';
+
+    // bam index
+    if ((bam_index = sam_index_load(bam_file, bam_path.string().c_str())) == nullptr) {
+        throw runtime_error("ERROR: Cannot open index for bam file: " + bam_path.string() + "\n");
+    }
+    cerr << t << "sam_index_load" << '\n';
+
+    // bam header
+    if ((bam_header = sam_hdr_read(bam_file)) == nullptr){
+        throw runtime_error("ERROR: Cannot open header for bam file: " + bam_path.string() + "\n");
+    }
+    cerr << t << "sam_hdr_read" << '\n';
+
+    hts_name2id_f getid = (hts_name2id_f)bam_name2id;
+    int r_count;
+
+    hts_reglist_t* reglist = hts_reglist_create(regions.data(), int(regions.size()), &r_count, bam_header,  getid);
+
+    cerr << t << "hts_reglist_create" << '\n';
+
+    hts_itr_t *itr = sam_itr_regions(bam_index, bam_header, reglist, r_count);
+
+    cerr << t << "sam_itr_regions" << '\n';
+
+    while (sam_itr_next(bam_file, itr, alignment) >= 0) {
+        if (alignment->core.tid < 0) {
+            continue;
+        }
+
+        // Placeholder code to demonstrate working properly
+        string read_name = bam_get_qname(alignment);
+        string ref_name = bam_header->target_name[alignment->core.tid];
+        auto pos = alignment->core.pos;
+        auto len = bam_cigar2rlen(int(alignment->core.n_cigar), bam_get_cigar(alignment));
+
+//        cerr << ref_name << ' ' << pos << ' ' << pos + len << ' ' << read_name << '\n';
+    }
+
+    cerr << t << "Done iterating" << '\n';
+
+    bam_destroy1(alignment);
+    hts_itr_destroy(itr);
+    bam_hdr_destroy(bam_header);
+    hts_close(bam_file);
+    hts_idx_destroy(bam_index);
+}
+
+
+//
+void iterate_regions_manually(path bam_path, vector<char*>& regions){
+    GoogleAuthenticator authenticator;
+    authenticator.update();
+
+    samFile* bam_file;
+    bam_hdr_t* bam_header;
+    hts_idx_t* bam_index;
+    bam1_t* alignment;
+
+    bam_file = nullptr;
+    bam_index = nullptr;
+    alignment = bam_init1();
+
+    Timer t;
+    cerr << t << "Begin" << '\n';
+
+    if ((bam_file = hts_open(bam_path.string().c_str(), "r")) == nullptr) {
+        throw runtime_error("ERROR: Cannot open bam file: " + bam_path.string());
+    }
+    cerr << t << "hts_open" << '\n';
+
+    // bam index
+    if ((bam_index = sam_index_load(bam_file, bam_path.string().c_str())) == nullptr) {
+        throw runtime_error("ERROR: Cannot open index for bam file: " + bam_path.string() + "\n");
+    }
+    cerr << t << "sam_index_load" << '\n';
+
+    // bam header
+    if ((bam_header = sam_hdr_read(bam_file)) == nullptr){
+        throw runtime_error("ERROR: Cannot open header for bam file: " + bam_path.string() + "\n");
+    }
+    cerr << t << "sam_hdr_read" << '\n';
+    hts_itr_t* itr;
+
+    for (auto region: regions) {
+        itr = sam_itr_querys(bam_index, bam_header, region);
+
+        while (sam_itr_next(bam_file, itr, alignment) >= 0) {
+            if (alignment->core.tid < 0) {
+                continue;
+            }
+
+            string read_name = bam_get_qname(alignment);
+            string ref_name = bam_header->target_name[alignment->core.tid];
+            auto pos = alignment->core.pos;
+            auto len = bam_cigar2rlen(int(alignment->core.n_cigar), bam_get_cigar(alignment));
+
+//            cerr << ref_name << ' ' << pos << ' ' << pos + len << ' ' << read_name << '\n';
+        }
+
+        hts_itr_destroy(itr);
+    }
+
+    cerr << t << "Done iterating" << '\n';
+
+    bam_destroy1(alignment);
+    bam_hdr_destroy(bam_header);
+    hts_close(bam_file);
+    hts_idx_destroy(bam_index);
 }
 
 
@@ -135,7 +273,12 @@ void test(){
         cerr << regions[i] << '\n';
     }
 
+    cerr << "Iterating chr20" << '\n';
+    iterate_region(bam_path, "chr20");
+    cerr << "Iterating 2000 regions with hts_iter_regions" << '\n';
     iterate_regions(bam_path, regions);
+    cerr << "Iterating 2000 regions with hts_iter_querys loop" << '\n';
+    iterate_regions_manually(bam_path, regions);
 }
 
 
