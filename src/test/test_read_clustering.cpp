21143cd86e4b0e32e2f2dd9cd66023fee2c35066
rlorigro <rlorigro@ucsc.edu>
2023-11-15 13:34:22 -0800

    Clustering optimizer working

--- src/test/test_read_clustering.cpp
+++ src/test/test_read_clustering.cpp
@@ -44,11 +44,11 @@ public:
     // Edges where the left node is the parent
     unordered_map <pair<int64_t,int64_t>, BoolVar> edges;
     unordered_map <int64_t, BoolVar> is_parent;
-    unordered_map <int64_t, BoolVar> is_child;
 
+    // Cost of all edges assigned
     LinearExpr cost_d;
 
-    // Will be left default initialized in the unary objective d_model
+    // Cost of adding parents
     LinearExpr cost_n;
 };
 
@@ -83,43 +83,84 @@ int main(){
         Variables vars;
 
         transmap.for_each_read_of_sample(sample_id, [&](int64_t id) {
+            // Construct indicator booleans which represent parent status of each node
             auto& p = vars.is_parent.emplace(id, model.NewBoolVar()).first->second;
-            auto& c = vars.is_child.emplace(id, model.NewBoolVar()).first->second;
 
-            // Any node can only be a parent or a child, one implies NOT the other
-            // TODO: Does this need its reciprocal??
-            model.AddEquality(p,Not(c));
-
+            // Keep track of the total number of parents assigned
+            // TODO: currently unused
             vars.cost_n += p;
         });
 
-        // TODO use BFS/connected components to enumerate edges instead, some edges won't be present
+        unordered_map<int64_t, LinearExpr> child_edge_sums;
+        unordered_map<int64_t, LinearExpr> parent_edge_sums;
+
+        int64_t n_reads = 0;
+        int64_t n_edges = 0;
+
+        // TODO use BFS/connected components to enumerate edges instead, some edges won't be present in practice
         transmap.for_each_read_of_sample(sample_id, [&](int64_t id_a){
+            cerr << id_a << ' ' << transmap.get_node(id_a).name << '\n';
+
             transmap.for_each_read_of_sample(sample_id, [&](int64_t id_b){
+                if (id_a == id_b){
+                    return;
+                }
+
                 auto [success,w] = transmap.try_get_edge_weight(id_a, id_b);
 
                 if (not success){
                     return;
                 }
 
+                // Choosing any edge starting with id_a implies that id_a is parent and id_b is child
                 pair<int64_t,int64_t> p = {id_a, id_b};
-                auto& result = vars.edges.emplace(p, model.NewBoolVar()).first->second;
+                auto& e = vars.edges.emplace(p, model.NewBoolVar()).first->second;
 
-                // Choosing this edge implies that the left is parent and the right is child
-                auto& p_a = vars.is_parent.at(id_a);
-                auto& c_b = vars.is_child.at(id_b);
+                cerr << '\t' <<  transmap.get_node(id_a).name << "->" << transmap.get_node(id_b).name << ' ' << w << '\n';
 
-                // TODO: FIX THIS, need to change to at least one, not a strict equality
-                model.AddEquality(result, p_a);
-                model.AddEquality(result, c_b);
+                // Keep track of which edges would make id_a a parent
+                parent_edge_sums[id_a] += e;
 
-                vars.cost_d += w*p_a;
+                // Keep track of which edges would make id_b a child
+                child_edge_sums[id_b] += e;
+
+                // Cost of selecting an edge
+                vars.cost_d += int64_t(w)*e;
+                n_edges++;
             });
+
+            n_reads++;
         });
 
-        model.AddGreaterThan(vars.cost_n, 0);
+        transmap.for_each_read_of_sample(sample_id, [&](int64_t id){
+            auto& p = vars.is_parent.at(id);
+
+            auto& p_sum = parent_edge_sums.at(id);
+            auto& c_sum = child_edge_sums.at(id);
+
+            // If there is at least one edge from id to another node, it is a parent node
+            model.AddGreaterThan(p_sum, 0).OnlyEnforceIf(p);
+            model.AddLessOrEqual(p_sum, 0).OnlyEnforceIf(Not(p));
+
+            // Constraint that being a parent node excludes possibility of incoming child edges
+            auto c = model.NewBoolVar();
+            model.AddEquality(c_sum, 1).OnlyEnforceIf(c);
+            model.AddEquality(c_sum, 0).OnlyEnforceIf(Not(c));
+            model.AddImplication(p, Not(c));
+
+            // Constraint that multiple edges cannot point to a child
+            model.AddLessOrEqual(c_sum, 1);
+
+            // Constraint that a node must be covered by an edge
+            model.AddGreaterThan(c_sum + p_sum, 0);
+        });
+
+        cerr << "Number of reads: " << n_reads << '\n';
+        cerr << "Number of edges: " << n_edges << '\n';
+
         model.Minimize(vars.cost_d);
 
+        cerr << '\n';
 
         const CpSolverResponse response = Solve(model.Build());
 
@@ -136,14 +177,7 @@ int main(){
                 if (is_assigned){
                     results.insert(name + ": parent");
                 }
-            }
-
-            // Iterate the child assignment variables and print them
-            for (const auto& [id,var]: vars.is_child){
-                auto name = transmap.get_node(id).name;
-
-                bool is_assigned = (SolutionIntegerValue(response, var) == 1);
-                if (is_assigned){
+                else{
                     results.insert(name + ": child");
                 }
             }
@@ -153,9 +187,10 @@ int main(){
                 auto [a,b] = e;
                 auto name_a = transmap.get_node(a).name;
                 auto name_b = transmap.get_node(b).name;
+                auto [success, w] = transmap.try_get_edge_weight(a,b);
 
                 bool is_assigned = (SolutionIntegerValue(response, var) == 1);
-                cerr << is_assigned << ' ' << name_a << "->" << name_b << '\n';
+                cerr << is_assigned << ' ' << name_a << "->" << name_b << ' ' << w << '\n';
             }
 
             for (auto& item: results){
