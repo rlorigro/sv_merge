38838e2284a95e85b79ad3c613417e5ca42326bf
rlorigro <rlorigro@ucsc.edu>
2023-12-18 09:11:06 -0800

    Implement simpler overlapper

--- inc/windows.hpp
+++ inc/windows.hpp
@@ -9,4 +9,6 @@ namespace sv_merge{
 
 void construct_windows_from_vcf_and_bed(path tandem_bed, path vcf, int32_t flank_length, int32_t interval_max_length, vector<Region>& regions);
 
+void get_overlapping_components(vector <pair <interval_t, bool> >& labeled_intervals, vector <interval_t>& result);
+
 }
--- src/windows.cpp
+++ src/windows.cpp
@@ -1,6 +1,6 @@
 #pragma once
 
-#include "IntervalGraph.hpp"
+#include "windows.hpp"
 #include "VcfReader.hpp"
 #include "bed.hpp"
 
@@ -13,20 +13,20 @@ void construct_windows_from_vcf_and_bed(path tandem_bed, path vcf, int32_t flank
     vcf_reader.progress_n_lines = 100'000;
     vcf_reader.min_sv_length = 20;
 
-    coord_t coord;
-    labeled_interval_t interval;
     unordered_set<uint32_t> sample_ids;
     unordered_set<string> sample_names;
 
-    unordered_map<string,vector<labeled_interval_t> > contig_intervals;
+    coord_t coord;
+    interval_t interval;
+
+    unordered_map<string,vector <pair <interval_t, bool> > > contig_intervals;
 
     cerr << "Reading BED... " << '\n';
     // Add every tandem region with a generic name as the label
-    interval.second.emplace("_tandem_");
     for_region_in_bed_file(tandem_bed, [&](const Region& r){
-        interval.first.first = r.start;
-        interval.first.second = r.stop + flank_length;
-        contig_intervals[r.name].emplace_back(interval);
+        interval.first = r.start;
+        interval.second = r.stop;
+        contig_intervals[r.name].emplace_back(interval, true);
     });
 
     cerr << "Reading VCF... " << '\n';
@@ -40,14 +40,7 @@ void construct_windows_from_vcf_and_bed(path tandem_bed, path vcf, int32_t flank
 
         r.get_reference_coordinates(true, coord);
 
-        sample_names.clear();
-        for (auto id: sample_ids){
-            sample_names.emplace(vcf_reader.sample_ids[id]);
-        }
-
-        coord.second += flank_length;
-
-        contig_intervals[r.chrom].emplace_back(coord, sample_names);
+        contig_intervals[r.chrom].emplace_back(coord, false);
     });
 
     cerr << "Computing intervals... " << '\n';
@@ -56,9 +49,9 @@ void construct_windows_from_vcf_and_bed(path tandem_bed, path vcf, int32_t flank
     for (auto& [contig, intervals]: contig_intervals){
         cerr << "\tStarting: " << contig << '\n';
 
-        // Iterate the VCF file and construct a vector of labeled intervals for the IntervalGraph
-        // Need to append `interval_padding` onto intervals and then subtract it afterwards (if desired)
-        IntervalGraph<string> g(intervals);
+        vector<interval_t> components;
+
+        get_overlapping_components(intervals, components);
 
         g.for_each_connected_component_interval([&](interval_t& interval, unordered_set<string>& values){
             if (interval.second - interval.first > interval_max_length){
@@ -69,5 +62,59 @@ void construct_windows_from_vcf_and_bed(path tandem_bed, path vcf, int32_t flank
         });
     }
 }
+
+
+void get_overlapping_components(vector <pair <interval_t, bool> >& labeled_intervals, vector <interval_t>& result, int32_t min_gap_length){
+    // How to sort labeled intervals
+    auto left_comparator = [](const pair <interval_t, bool>& a, const pair <interval_t, bool>& b){
+        return a.first.first < b.first.first;
+    };
+
+    sort(labeled_intervals.begin(), labeled_intervals.end(), left_comparator);
+
+    result.clear();
+    result.emplace_back(labeled_intervals.front().first);
+
+    bool has_non_tandem_interval;
+
+    for (auto& [interval,is_tandem] : labeled_intervals){
+        if (interval.first > interval.second){
+            throw runtime_error("ERROR: interval start is greater than interval stop: " + to_string(interval.first) + ',' + to_string(interval.second));
+        }
+
+        interval.second += min_gap_length;
+
+        // If the current interval has no overlap with any of the previous intervals, start new component
+        if (interval.first > result.back().second){
+            // If the only intervals in this component are tandem intervals, just overwrite it, because it is not needed
+            if (not has_non_tandem_interval){
+                result.back() = interval;
+            }
+            else{
+                // Otherwise, start a new interval
+                result.emplace_back(interval);
+
+                // Trim the buffer off the completed component
+                result.back().second -= min_gap_length;
+            }
+
+            has_non_tandem_interval = false;
+        }
+
+        // Maintain the maximum bound of this component
+        if (interval.second > result.back().second){
+            result.back().second = interval.second;
+        }
+
+        // Make sure there is at least one non-tandem interval
+        if (not is_tandem){
+            has_non_tandem_interval = true;
+        }
+
+        // Return the stored interval to original state
+        interval.second -= min_gap_length;
+    }
+}
+
 
 }
