601b82573091614f350cdae74af953c7eddd63b6
rlorigro <rlorigro@ucsc.edu>
2024-01-05 15:32:44 -0800

    Add bitmagic demo/test

---
+++ src/test/test_bitmagic.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <stdexcept>
+
+using std::runtime_error;
+using std::cerr;
+
+#include "bitmagic/inc/bm.h"
+#include "bitmagic/inc/bm64.h"
+#include "bitmagic/inc/bmsparsevec.h"
+#include "bitmagic/inc/bmsparsevec_serial.h"
+#include "bitmagic/inc/bmundef.h"
+
+using namespace bm;
+
+int main(){
+//    const char* s = "ATGTCNNNNNTATA";
+//    sparse_vector<> sv;
+//    {
+//        svector_u32::back_insert_iterator bi = sv.get_back_inserter();
+//        for (unsigned i = 0; s[i]; ++i)
+//        {
+//            bi = DNA2int(s[i]);
+//        }
+//        bi.flush();
+//    }
+//    sv.optimize(); // this will RLE compress the sparse (N) plane
+
+    return 0;
+}
+
