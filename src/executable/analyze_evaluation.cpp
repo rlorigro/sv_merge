9e8be693af7ede050d2e3bc200e32fe149296cc0
fcunial <fcunial@broadinstitute.org>
2024-01-22 18:38:53 -0500

    updates

---
+++ src/executable/analyze_evaluation.cpp
@@ -0,0 +1,40 @@
+#include "Filesystem.hpp"
+
+using ghc::filesystem::path;
+
+#include <iostream>
+#include <fstream>
+#include <exception>
+#include <stdexcept>
+
+using std::string;
+using std::vector;
+
+using namespace sv_merge;
+
+
+
+/**
+ * Directory structure:
+ *
+ *
+ * @param directory
+ */
+void collect(const path& directory, vector<string>& tools) {
+
+    for (const auto& tool: tools) {
+
+    }
+
+
+}
+
+
+
+
+
+
+
+int main (int argc, char* argv[]) {
+
+}
