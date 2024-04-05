945e3b8c91703b1932d6145da6dd665e3481fa4c
rlorigro <rlorigro@ucsc.edu>
2024-04-04 20:34:24 -0700

    More better

--- scripts/train_vcf_filter_model.py
+++ scripts/train_vcf_filter_model.py
@@ -30,7 +30,7 @@ import os
 print("USING pytorch VERSION: ", torch.__version__)
 
 
-def train(model, train_loader, test_loader, optimizer, loss_fn, epochs=5):
+def train(model, train_loader, test_loader, optimizer, loss_fn, epochs):
     train_losses = list()
     test_losses = list()
 
@@ -38,10 +38,19 @@ def train(model, train_loader, test_loader, optimizer, loss_fn, epochs=5):
     _, _, starting_test_loss = test(model=model, loader=test_loader, loss_fn=torch.nn.BCELoss())
     model.train()
 
+    n_total_positive = 0
+    n_total_negative = 0
+
     batch_index = 0
     for e in range(epochs):
         for x, y in train_loader:
             loss = train_batch(model=model, x=x, y=y, optimizer=optimizer, loss_fn=loss_fn)
+
+            n_positive = sum(y.data.numpy())
+            n_negative = len(y) - n_positive
+
+            n_total_positive += n_positive
+            n_total_negative += n_negative
 
             train_losses.append(loss)
 
@@ -61,6 +70,8 @@ def train(model, train_loader, test_loader, optimizer, loss_fn, epochs=5):
 
         print("Epoch: ", e+1)
         print("Batches: ", batch_index)
+        print("n_total_positive: ", n_total_positive)
+        print("n_total_negative: ", n_total_negative)
 
     return train_losses, test_losses
 
@@ -124,7 +135,7 @@ def test_batch(model, x, y):
     return y, y_predict
 
 
-def plot_loss(train_losses, test_losses, show=True):
+def plot_loss(train_losses, test_losses):
     fig = pyplot.gcf()
     fig.set_size_inches(8,6)
     ax = pyplot.axes()
@@ -135,24 +146,41 @@ def plot_loss(train_losses, test_losses, show=True):
     pyplot.plot(x_loss, test_losses, label="test")
 
 
-def run(dataset_train, dataset_test):
-    # Batch size is the number of training examples used to calculate each iteration's gradient
-    batch_size_train = 8192
-
-    data_loader_train = DataLoader(dataset=dataset_train, batch_size=batch_size_train, shuffle=True)
-    data_loader_test = DataLoader(dataset=dataset_test, batch_size=len(dataset_test), shuffle=False)
-
+def run(dataset_train, dataset_test, downsample=False):
     # Define the hyperparameters
     learning_rate = 1e-3
+    weight_decay = 1e-4
+
+    n_epochs = 8
+
+    goal_batch_size = 2048
+
+    # Batch size is the number of training examples used to calculate each iteration's gradient
+    batch_size_train = min(goal_batch_size, len(dataset_train))
+
+    if downsample:
+        minority_class_size = min(sum(dataset_train.y_data), len(dataset_train.y_data) - sum(dataset_train.y_data))
+        batch_size_train = int(min(goal_batch_size, minority_class_size*2))
+
+    print("Using batch size: ", batch_size_train)
+
+    data_loader_train = None
+
+    if downsample:
+        weight_tensor = compute_downsampling_weight_tensor(dataset_train.y_data)
+        sampler = torch.utils.data.sampler.WeightedRandomSampler(weight_tensor, len(weight_tensor))
+        data_loader_train = DataLoader(dataset=dataset_train, batch_size=batch_size_train, sampler=sampler)
+    else:
+        data_loader_train = DataLoader(dataset=dataset_train, batch_size=batch_size_train, shuffle=True)
+
+    data_loader_test = DataLoader(dataset=dataset_test, batch_size=len(dataset_test), shuffle=False)
 
     input_size = len(dataset_train[0][0])
     print("using input size: ", input_size)
     shallow_model = ShallowLinear(input_size=input_size)
 
-    n_epochs = 16
-
     # Initialize the optimizer with above parameters
-    optimizer = optim.AdamW(shallow_model.parameters(), lr=learning_rate, weight_decay=1e-4)
+    optimizer = optim.AdamW(shallow_model.parameters(), lr=learning_rate, weight_decay=weight_decay)
 
     # Define the loss function
     loss_fn = nn.BCEWithLogitsLoss()  # cross entropy
@@ -168,57 +196,75 @@ def run(dataset_train, dataset_test):
     return train_losses, test_losses, y_predict.squeeze(), y_test, shallow_model
 
 
+def compute_downsampling_weight_tensor(y_data):
+    n_total = len(y_data)
+    n_true = sum(y_data)
+    n_false = n_total - sum(y_data)
+    weight_tensor = torch.clone(y_data).detach()
+
+    # compute a weight such that the majority class will be downsampled by that weight to match the minority weight,
+    # and then construct a tensor of weights such that each element of the minority class has weight 1 and each element
+    # of the majority class has weight equal to the downsample weight
+    if n_true < n_false:
+        weight = n_true / n_false
+        weight_tensor[y_data == 0] = weight
+        weight_tensor[y_data == 1] = 1
+    else:
+        weight = n_false / n_true
+        weight_tensor[y_data == 1] = weight
+        weight_tensor[y_data == 0] = 1
+
+    return weight_tensor
+
+
+def downsample_test_data(y_true, y_predict, seed=37):
+    random.seed(seed)
+
+    n_true = sum(y_true)
+    n_false = len(y_true) - n_true
+
+    true_weight = min(1.0, float(n_false) / float(n_true))
+    false_weight = min(1.0, float(n_true) / float(n_false))
+
+    mask = numpy.zeros(len(y_true))
+
+    for i in range(len(y_true)):
+        if y_true[i] == 1 and random.random() <= true_weight:
+            mask[i] = 1
+        if y_true[i] == 0 and random.random() <= false_weight:
+            mask[i] = 1
+
+    y_true = y_true[mask == 1]
+    y_predict = y_predict[mask == 1]
+
+    return y_true, y_predict
+
+
 def main():
     output_path = "test_annotation.vcf"
 
-    # train_paths = [
-    #     "/home/ryan/data/test_hapestry/run/truvari_hg002_nist_tandem_max50kbp_chr1_ref/HG002_truvari_collapsed_chr1_norm_annotated_annotated.vcf",
-    #     "/home/ryan/data/test_hapestry/run/truvari_hg002_nist_tandem_max50kbp_chr2_ref/HG002_truvari_collapsed_chr2_norm_annotated_annotated.vcf",
-    #     "/home/ryan/data/test_hapestry/run/truvari_hg002_nist_tandem_max50kbp_chr3_ref/HG002_truvari_collapsed_chr3_norm_annotated_annotated.vcf",
-    #     "/home/ryan/data/test_hapestry/run/truvari_hg002_nist_tandem_max50kbp_chr4_ref/HG002_truvari_collapsed_chr4_norm_annotated_annotated.vcf",
-    #     "/home/ryan/data/test_hapestry/run/truvari_hg002_nist_tandem_max50kbp_chr5_ref/HG002_truvari_collapsed_chr5_norm_annotated_annotated.vcf",
-    #     "/home/ryan/data/test_hapestry/run/truvari_hg002_nist_tandem_max50kbp_chr8_ref/HG002_truvari_collapsed_chr8_norm_annotated_annotated.vcf",
-    #     "/home/ryan/data/test_hapestry/run/truvari_hg002_nist_tandem_max50kbp_chr9_ref/HG002_truvari_collapsed_chr9_norm_annotated_annotated.vcf",
-    #     "/home/ryan/data/test_hapestry/run/truvari_hg002_nist_tandem_max50kbp_chr10_ref/HG002_truvari_collapsed_chr10_norm_annotated_annotated.vcf",
-    # ]
-    #
-    # test_paths = [
-    #     "/home/ryan/data/test_hapestry/run/truvari_hg002_nist_tandem_max50kbp_chr6_ref/HG002_truvari_collapsed_chr6_norm_annotated_annotated.vcf",
-    #     "/home/ryan/data/test_hapestry/run/truvari_hg002_nist_tandem_max50kbp_chr7_ref/HG002_truvari_collapsed_chr7_norm_annotated_annotated.vcf",
-    # ]
-
-    train_paths = [
-        "/home/ryan/data/test_hapestry/run/truvari_hg002_nist_tandem_max50kbp_benchnotate_50flank_no_skip_mum_chr1_ref/truvari_HG002_collapsed_NIST_benchnotated_confident_only_chr1_norm_annotated_annotated.vcf",
-        "/home/ryan/data/test_hapestry/run/truvari_hg002_nist_tandem_max50kbp_benchnotate_50flank_no_skip_mum_chr2_ref/truvari_HG002_collapsed_NIST_benchnotated_confident_only_chr2_norm_annotated_annotated.vcf",
-        "/home/ryan/data/test_hapestry/run/truvari_hg002_nist_tandem_max50kbp_benchnotate_50flank_no_skip_mum_chr3_ref/truvari_HG002_collapsed_NIST_benchnotated_confident_only_chr3_norm_annotated_annotated.vcf",
-        "/home/ryan/data/test_hapestry/run/truvari_hg002_nist_tandem_max50kbp_benchnotate_50flank_no_skip_mum_chr4_ref/truvari_HG002_collapsed_NIST_benchnotated_confident_only_chr4_norm_annotated_annotated.vcf",
-        "/home/ryan/data/test_hapestry/run/truvari_hg002_nist_tandem_max50kbp_benchnotate_50flank_no_skip_mum_chr5_ref/truvari_HG002_collapsed_NIST_benchnotated_confident_only_chr5_norm_annotated_annotated.vcf",
-        "/home/ryan/data/test_hapestry/run/truvari_hg002_nist_tandem_max50kbp_benchnotate_50flank_no_skip_mum_chr9_ref/truvari_HG002_collapsed_NIST_benchnotated_confident_only_chr9_norm_annotated_annotated.vcf",
-        "/home/ryan/data/test_hapestry/run/truvari_hg002_nist_tandem_max50kbp_benchnotate_50flank_no_skip_mum_chr10_ref/truvari_HG002_collapsed_NIST_benchnotated_confident_only_chr10_norm_annotated_annotated.vcf",
-        "/home/ryan/data/test_hapestry/run/truvari_hg002_nist_tandem_max50kbp_benchnotate_50flank_no_skip_mum_chr11_ref/truvari_HG002_collapsed_NIST_benchnotated_confident_only_chr11_norm_annotated_annotated.vcf",
-        "/home/ryan/data/test_hapestry/run/truvari_hg002_nist_tandem_max50kbp_benchnotate_50flank_no_skip_mum_chr12_ref/truvari_HG002_collapsed_NIST_benchnotated_confident_only_chr12_norm_annotated_annotated.vcf",
-        "/home/ryan/data/test_hapestry/run/truvari_hg002_nist_tandem_max50kbp_benchnotate_50flank_no_skip_mum_chr13_ref/truvari_HG002_collapsed_NIST_benchnotated_confident_only_chr13_norm_annotated_annotated.vcf",
-        "/home/ryan/data/test_hapestry/run/truvari_hg002_nist_tandem_max50kbp_benchnotate_50flank_no_skip_mum_chr14_ref/truvari_HG002_collapsed_NIST_benchnotated_confident_only_chr14_norm_annotated_annotated.vcf",
-        "/home/ryan/data/test_hapestry/run/truvari_hg002_nist_tandem_max50kbp_benchnotate_50flank_no_skip_mum_chr15_ref/truvari_HG002_collapsed_NIST_benchnotated_confident_only_chr15_norm_annotated_annotated.vcf",
-        "/home/ryan/data/test_hapestry/run/truvari_hg002_nist_tandem_max50kbp_benchnotate_50flank_no_skip_mum_chr16_ref/truvari_HG002_collapsed_NIST_benchnotated_confident_only_chr16_norm_annotated_annotated.vcf",
-        "/home/ryan/data/test_hapestry/run/truvari_hg002_nist_tandem_max50kbp_benchnotate_50flank_no_skip_mum_chr17_ref/truvari_HG002_collapsed_NIST_benchnotated_confident_only_chr17_norm_annotated_annotated.vcf",
-    ]
-
-    test_paths = [
-        "/home/ryan/data/test_hapestry/run/truvari_hg002_nist_tandem_max50kbp_benchnotate_50flank_no_skip_mum_chr6_ref/truvari_HG002_collapsed_NIST_benchnotated_confident_only_chr6_norm_annotated_annotated.vcf",
-        "/home/ryan/data/test_hapestry/run/truvari_hg002_nist_tandem_max50kbp_benchnotate_50flank_no_skip_mum_chr7_ref/truvari_HG002_collapsed_NIST_benchnotated_confident_only_chr7_norm_annotated_annotated.vcf",
-        "/home/ryan/data/test_hapestry/run/truvari_hg002_nist_tandem_max50kbp_benchnotate_50flank_no_skip_mum_chr8_ref/truvari_HG002_collapsed_NIST_benchnotated_confident_only_chr8_norm_annotated_annotated.vcf",
+    vcf_paths = [
+        "/home/ryan/data/test_hapestry/vcf/hprc_8x_annotation_test/hapestry_and_truvari_and_sniffles/HG002_truvari_hapestry_sniffles_annotated.vcf.gz",
+        "/home/ryan/data/test_hapestry/vcf/hprc_8x_annotation_test/hapestry_and_truvari_and_sniffles/HG00438_truvari_hapestry_sniffles_annotated.vcf.gz",
+        "/home/ryan/data/test_hapestry/vcf/hprc_8x_annotation_test/hapestry_and_truvari_and_sniffles/HG00621_truvari_hapestry_sniffles_annotated.vcf.gz",
+        "/home/ryan/data/test_hapestry/vcf/hprc_8x_annotation_test/hapestry_and_truvari_and_sniffles/HG00673_truvari_hapestry_sniffles_annotated.vcf.gz",
+        "/home/ryan/data/test_hapestry/vcf/hprc_8x_annotation_test/hapestry_and_truvari_and_sniffles/HG00733_truvari_hapestry_sniffles_annotated.vcf.gz",
     ]
 
     # truth_info_name = "Hapestry"
-    truth_info_name = "TRUVARI_HG002_NIST"
+    truth_info_name = "TruvariBench_TP"
 
-    dataset_train = VcfDataset(vcf_paths=train_paths, truth_info_name=truth_info_name)
-    dataset_test = VcfDataset(vcf_paths=test_paths, truth_info_name=truth_info_name)
-    # dataset_test_hapestry = VcfDataset(vcf_paths=test_paths, truth_info_name="Hapestry")
+    annotation_name = "Hapestry"
+    # annotation_name = "Sniffles"
 
-    print(dataset_train[0])
-    print(dataset_train[1])
+    train_contigs = {"chr1", "chr2", "chr4", "chr5", "chr9", "chr10", "chr11", "chr12", "chr13", "chr14", "chr15", "chr16", "chr17", "chr18", "chr21", "chr22", "chrX"}
+    test_contigs = {"chr6", "chr7", "chr8"}
+
+    # train_contigs = {"chr1", "chr2"}
+    # test_contigs = {"chr6"}
+
+    dataset_train = VcfDataset(vcf_paths=vcf_paths, truth_info_name=truth_info_name, annotation_name=annotation_name, contigs=train_contigs)
+    dataset_test = VcfDataset(vcf_paths=vcf_paths, truth_info_name=truth_info_name, annotation_name=annotation_name, contigs=test_contigs)
 
     # Print information on the train and test datasets
     print('Train datapoints: ', len(dataset_train))
@@ -226,7 +272,9 @@ def main():
     print('Input shape: ', dataset_train[0][0].shape)
     print('Output shape: ', dataset_train[0][1].shape)
 
-    train_losses, test_losses, y_predict, y_true, model = run(dataset_train=dataset_train, dataset_test=dataset_test)
+    train_losses, test_losses, y_predict, y_true, model = run(dataset_train=dataset_train, dataset_test=dataset_test, downsample=True)
+
+    y_true, y_predict = downsample_test_data(y_true, y_predict)
 
     # Get the current date and time
     now = datetime.now()
@@ -252,14 +300,14 @@ def main():
     fig.set_size_inches(8,6)
     axes = pyplot.axes()
 
-    axes = plot_roc_curve(y_true=y_true, y_predict=y_predict, axes=axes, label="trained on " + truth_info_name + " labels")
+    axes = plot_roc_curve(y_true=y_true, y_predict=y_predict, axes=axes, label="trained on " + truth_info_name + " truth labels and " + annotation_name + " features")
 
     # Generate a legend for axes and force bottom right location
     axes.legend(loc="lower right")
-    pyplot.savefig("roc_curve_" + truth_info_name + ".png", dpi=200)
+    pyplot.savefig("roc_curve_" + truth_info_name + "_" + annotation_name + ".png", dpi=200)
 
-    axes = plot_confusion(y_true=y_true, y_predict=y_predict, title="trained on " + truth_info_name + " labels", threshold=0.5)
-    pyplot.savefig("confusion_" + truth_info_name + ".png", dpi=200)
+    axes = plot_confusion(y_true=y_true, y_predict=y_predict, title="trained on " + truth_info_name + " truth labels and " + annotation_name + " features", threshold=0.5)
+    pyplot.savefig("confusion_" + truth_info_name + "_" + annotation_name + ".png", dpi=200)
 
     pyplot.show()
     pyplot.close()
