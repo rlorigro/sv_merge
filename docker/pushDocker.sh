892165c4c9b0e474e5f34e75d0489556c2a163eb
rlorigro <28764332+rlorigro@users.noreply.github.com>
2024-07-11 14:41:06 -0700

    Bump version and make pushDocker.sh better

--- docker/pushDocker.sh
+++ docker/pushDocker.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
-set -euo
-TAG="annotate"
+set -euxo
+
+if [ $# -eq 0 ]; then
+  echo "Need to provide tag for the docker image."
+  exit 1
+fi
+
+TAG="$1"
+
 docker build --progress=plain -t fcunial/hapestry .
 docker tag fcunial/hapestry fcunial/hapestry:${TAG}
 docker push fcunial/hapestry:${TAG}
